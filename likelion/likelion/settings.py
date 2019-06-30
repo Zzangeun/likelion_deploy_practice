d66ae0cb7db722259aaffe90672549207efea8d6
Zzangeun <tkddms4199@gmail.com>
2019-06-30 18:40:22 +0900

    second_

--- likelion/likelion/settings.py
+++ likelion/likelion/settings.py
@@ -20,10 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'xf45ktc4b4l*@f-#gdrg4bebcqm*(c7pc560x-%p%2enn$$1mh'
+# SECRET_KEY = 'xf45ktc4b4l*@f-#gdrg4bebcqm*(c7pc560x-%p%2enn$$1mh'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = []
 
